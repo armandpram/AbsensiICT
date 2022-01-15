f26d013571ccf36d273c1bb1241f299a2b8715cf
Than1st <1911500641@student.budiluhur.ac.id>
2022-01-15 17:54:47 +0700

    Kurang include
    kurang include di index,php

--- AbsenICT/index.php
+++ AbsenICT/index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu | Absensi LAB ICT</title>
+    <?php include "header.php"; ?>
 </head>
 <body>
     <?php include "dashboard.php"; ?>
