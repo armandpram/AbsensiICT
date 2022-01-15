9f18da65cb0944cade3a8f6226ff89b6b800454f
Than1st <1911500641@student.budiluhur.ac.id>
2022-01-15 18:33:32 +0700

    Edit
    Dashboard sama Header

--- AbsenICT/dashboard.php
+++ AbsenICT/dashboard.php
@@ -1,12 +1,14 @@
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a href="home.php"><img src="img/labict.png" width="50px"></a>
+            <a href="index.php"><img src="img/labict.png" width="50px"></a>
         </div>
         <ul class="nav navbar-nav">
-            <li><a href="dataasisten.php">DATA ASISTEN</a></li>
-            <li><a href="absensi.php">DATA ABSENSI</a></li>
-            <li><a href="scan.php">ABSEN DISINI</a></li>
+            <li><a href="asisten.php">Data Asisten</a></li>
+            <li><a href="calas.php">Data Calas</a></li>
+            <li><a href="absensi.php">Rekap Absensi</a></li>
+            <li><a href="scan.php">Absen Disini</a></li>
         </ul>
     </div>
 </nav>
+<?php include "koneksi.php"; ?>
--- AbsenICT/header.php
+++ AbsenICT/header.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="css/style.css" type="text/css">
 <script type="text/javasrpit" src="jquery/jquery.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
