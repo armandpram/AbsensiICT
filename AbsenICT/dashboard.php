5770a6e00db28a83b579624ee3b26fbbe8c3dc74
armandpram <60423726+armandpram@users.noreply.github.com>
2022-01-15 17:22:21 +0700

    Add files via upload

---
+++ AbsenICT/dashboard.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="?" class="navbar-brand">ASBENSI LAB ICT</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="home.php"><img src="img/labict.png" width="100px"></a></li>
+            <li><a href="dataasisten.php">DATA ASISTEN</a></li>
+            <li><a href="absensi.php">DATA ABSENSI</a></li>
+            <li><a href="scan.php">ABSEN DISINI</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ AbsenICT/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu | Absensi LAB ICT</title>
+</head>
+<body>
+    <?php include "dashboard.php"; ?>
+
+    <!-- isinya -->
+    <div class="container-fluid" style="padding-top: 10%; text-align:center">
+        <img src="" alt="" srcset="">
+    </div>
+</body>
+</html>
